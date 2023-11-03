ea314932841435c566a4719fffaee67c07028550
juan Matias Brochero <69097148+jmatiasbro@users.noreply.github.com>
2023-11-03 18:51:37 -0300

    se agrego la carpeta de mailClient

--- ESCMB.HybridDDDArchitecture/ESCMB.Application/UseCases/Client/Commands/CreateClient/CreateClientHandler.cs
+++ ESCMB.HybridDDDArchitecture/ESCMB.Application/UseCases/Client/Commands/CreateClient/CreateClientHandler.cs
@@ -3,6 +3,7 @@ using ESCMB.Application.Common;
 using ESCMB.Application.DomainEvents;
 using ESCMB.Application.Exceptions;
 using ESCMB.Application.Repositories.Sql;
+using ESCMB.Domain.MailAdapter;
 
 namespace ESCMB.Application.UseCases.Client.Commands.CreateClient
 {
@@ -31,6 +32,16 @@ namespace ESCMB.Application.UseCases.Client.Commands.CreateClient
 
             await _eventPublisher.Publish(client.To<ClientCreated>(), cancellationToken).ConfigureAwait(false);
 
+           // Inicio de Envío mail
+            /* Inicio de la parametrización del mail */
+            string _fullName = $"{client.Nombre + " " + client.Apellido}";
+            string _linkButton = $"http://localhost:4200/put/{id}";
+            string _subject = $"{client.Nombre}, desde PayMaster queremos confirmar tu registro";
+            string _body = $"<body>\r\n    <h1>¡Hola {_fullName}!</h1>\r\n    <p>Confirmá tu correo para continuar con el registro.</p>\r\n    <a href=\"{_linkButton}\" style=\"background-color: #007BFF; color: #ffffff; text-decoration: none; padding: 10px 20px; border-radius: 5px; display: inline-block;\">Confirmar</a>\r\n</body>";
+            /* Fin de la parametrización del mail */
+            MailClient mailClient = new MailClient(client.Email, _fullName, _subject, _body);
+            mailClient.SendMail();
+            // Inicio de Envío  mail
             return id;
         }
     }
---
+++ ESCMB.HybridDDDArchitecture/ESCMB.Domain/MailAdapter/MailClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESCMB.Domain.MailAdapter
+{
+    public class MailClient
+    {
+       
+            public string Host { get; private set; }
+            public int Port { get; private set; }
+            public bool EnableSsl { get; private set; }
+            public SmtpDeliveryMethod DeliveryMethod { get; private set; }
+            public bool UseDefaultCredentials { get; private set; }
+            public NetworkCredential Credentials { get; private set; }
+
+            // Atributos parametrizables
+            public string AddressMail { get; private set; } // Mail del destino
+            public string AddressName { get; private set; } // Nombre del destino
+            public string DynamicSubject { get; private set; } // Asunto dinámico
+            public string DynamicBody { get; private set; } // Cuerpo del Mail dinámico
+
+            public MailClient(string addressMail, string addressName, string dynamicSubject, string dynamicBody)
+            {
+                AddressMail = addressMail;
+                AddressName = addressName;
+                DynamicSubject = dynamicSubject;
+                DynamicBody = dynamicBody;
+            }
+
+            public void SendMail()
+            {
+                try
+                {
+                    var fromAddress = new MailAddress("andres030685@gmail.com", "PayMaster");
+                    var toAddress = new MailAddress(AddressMail, AddressName);
+                    const string fromPassword = "fqui gxps bdcr bfae";
+
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                    };
+
+                    using (var message = new MailMessage(fromAddress, toAddress)
+                    {
+                        Subject = DynamicSubject,
+                        Body = DynamicBody,
+                        IsBodyHtml = true
+                    })
+                    {
+                        smtp.Send(message);
+                    }
+
+                    Console.WriteLine("Correo enviado exitosamente.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al enviar el correo: " + ex.Message);
+                }
+            }
+        }
+
+
+    }
+
